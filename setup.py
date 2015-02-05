f65766c404b730f510053ec395081906e18c1b23
Igor Khrol <i_khrol@wargaming.net>
2015-02-05 09:09:38 +0300

    waiting==1.2.1

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-version = '1.0.2'
+version = '1.0.3'
 
 setup(
     name='webium',
@@ -25,7 +25,7 @@ setup(
     install_requires=[
         'selenium==2.43',
         'nose==1.3.3',
-        'waiting==1.2.0',
+        'waiting==1.2.1',
     ],
     entry_points={
         'nose.plugins': ['browser_closer = webium.plugins.browser_closer:BrowserCloserPlugin'],
